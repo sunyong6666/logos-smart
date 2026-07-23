53dd6d7a8bda44ce0209c60124ebc8df1633b8d6
sunyong6666 <1824036028@qq.com>
2026-07-23 15:05:19 +0800

    更新 block/geekServo.ts

--- block/geekServo.ts
+++ block/geekServo.ts
@@ -2,11 +2,11 @@
 
 namespace LogosSmart {
 
-    //% blockId=LogosSmart_Servo
-    //% block="Servo pin %num angle %value"
+    //% blockId=servo
+    //% block="servo pin %num angle %value"
     //% value.min=0 value.max=180
     //% group="Servo"
-    export function Servo4(num: enGPIOpin, value: number): void {
+    export function servo(num: enGPIOpin, value: number): void {
         pins.servoWritePin(num, value)
     }
 
